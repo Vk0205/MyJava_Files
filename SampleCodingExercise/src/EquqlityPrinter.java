1b710e7f046fed5c11a9af7fcefb0e8d63362a9b
Vk0205 <108221507+Vk0205@users.noreply.github.com>
2023-02-14 06:08:39 +0530

    Update EquqlityPrinter.java

--- SampleCodingExercise/src/EquqlityPrinter.java
+++ SampleCodingExercise/src/EquqlityPrinter.java
@@ -13,29 +13,29 @@ NOTES:
 
 public class IntEqualPrinter {
 
-  public static void printEqual(int fNumber, int sNumber, int tNumber) {
-  
-    if (fNumber < 0 || sNumber  0 || tNumber < 0) {
-    
-      System.out.pintln("Invalid Value");
-    
-    }
-    else if (fNumber == sNumber && fNumber == tNumber && sNumber == tNumber) {
-    
-      System.out.println("All numbers are equal");
-    
-    }
-    else if (fNumber != sNumber && fNumber != tNumber && sNumber != tNumber) {
-    
-      System.out.println("All numbers are different");
-    
-    }
-    else {
-    
-      System.out.println("Neither all are equal or different");
-    
-    }
-  
-  }
+    public static void printEqual(int fNumber, int sNumber, int tNumber) {
 
+        if (fNumber < 0 || sNumber < 0 || tNumber < 0) {
+
+            System.out.println("Invalid Value");
+
+        }
+        else if (fNumber == sNumber && fNumber == tNumber) {
+
+            System.out.println("All numbers are equal");
+
+        }
+        else if (fNumber != sNumber && fNumber != tNumber && sNumber != tNumber) {
+
+            System.out.println("All numbers are different");
+
+        }
+        else {
+
+            System.out.println("Neither all are equal or different");
+
+        }
+
+    }
+  
 }
